1333ce7ec6b1d1fcec607592d6d370aefefedb97
dale das <daledas@dales-MacBook-Air.local>
2023-08-21 20:59:02 -0400

    added trigger button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  var innerAnswer = "Answer Goes here"
+  const gptTrigger = async () => {
+      const API_KEY = process.env.REACT_APP_SECRET_KEY;
+      var gptQuestion = "Explain how to draft a good fantasy basketball team";
+      const options = {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: gptQuestion }],
+          max_tokens: 2000,
+        })
+  
+      }
+  
+      try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        console.log("Content Answer:")
+        console.log(data.choices[0].message.content)
+        document.getElementById("answerDiv").innerHTML = data.choices[0].message.content;
+      }
+  
+      catch (error){
+        console.error(error)
+  
+      }
+    };
+  
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      <button onClick={gptTrigger}> FANTASY BASKETBALL App Button  </button>
+      <br />
+      <p id="answerDiv" > answer goes here </p> 
+      </div>
+    );
+  }
+  
+  export default App;
